84205baa67cb827e1bf0a7e3fe20caab5bd2fd57
anamikajangid <anamikajangid12@gmail.com>
2023-09-15 22:45:52 +0530

    state

--- src/Component/Fromvalidate.js
+++ src/Component/Fromvalidate.js
@@ -1,28 +1,80 @@
 import React, { useState } from "react";
 
 const FormValidate = () => {
-  const [name, setName] = useState("");
-  const [upperCaseName, setUpperCase] = useState("");
+  // const [name, setName] = useState("");
+  // const [email,setEmail] = useState("");
+  // const [password,setPassword] = useState("");
+  // const [confirmPassword, setConfirmPassword] = useState("");
 
-  //   function makeItCapital() {
-  //     setName(name.toUpperCase());
-  //   }
-  function makeItCapital() {
-    setUpperCase(name.toUpperCase());
+  let [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  let [error, setError] = useState("");
+  let [success, setSuccess] = useState("");
+  let { name, email, password, confirmPassword } = user;
+  //console.log(user);
+  function implementSignup(event) {
+    event.preventDefault();
+
+    //validation......
+    if (!name || !email || !password || !confirmPassword) {
+      setError(" please Fill All the Fields");
+      setSuccess("");
+      return;
+    } else if (password !== confirmPassword) {
+      setError("Enter Wromg Password");
+      setSuccess("");
+      return;
+    }
+
+    setSuccess("form Submitted Successfully");
+    setError("");
   }
-
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Enter yout Name"
-        onChange={(e) => setName(e.target.value)}
-      />
-      {/* we can directly use as callback instead of making updateName function */}
-      {/* onChange={e =>setName(e.target.value)} */}
-      <button onClick={makeItCapital}>Convert To Uppercase</button>
-      {/* <p>Your Name is: {name}</p> */}
-      <p>Your Name is: {upperCaseName}</p>
+      {error && <h3 style={{ color: "red" }}> {error} </h3>}
+      {success && <h3 style={{ color: "green" }}> {success} </h3>}
+
+      <form onSubmit={implementSignup}>
+        <input
+          type="text"
+          placeholder="Enter your Name"
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+        />
+        <br />
+        <br />
+        <input
+          type="email"
+          placeholder="Enter Your Email"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        <br />
+        <br />
+        <input
+          type="password"
+          placeholder="Enter password"
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+        <br />
+        <br />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          onChange={(e) =>
+            setUser({ ...user, confirmPassword: e.target.value })
+          }
+        />
+        <br />
+        <br />
+
+        {/* we can directly use as callback instead of making updateName function */}
+        {/* onChange={e =>setName(e.target.value)} */}
+        <button type="submit">Submit</button>
+        {/* <p>Your Name is: {name}</p> */}
+      </form>
     </div>
   );
 };
